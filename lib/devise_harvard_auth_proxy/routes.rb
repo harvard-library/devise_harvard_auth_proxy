b2c29082f03f629757029ef369a12ee8462d30c0
Dave Mayo <dave_mayo@harvard.edu>
2015-09-09 09:17:27 -0400

    Removed conditional routing to support Rails 4
    As this software never had real Rails 2 support anyway,
    and as Rails 3 and Rails 4 use the same mechanism for routing,
    It is resolved that the theoretical space for future Rails 2 support
    is less valuable than actual Rails 4 support.

--- lib/devise_harvard_auth_proxy/routes.rb
+++ lib/devise_harvard_auth_proxy/routes.rb
@@ -1,30 +1,17 @@
-if ActionController::Routing.name =~ /ActionDispatch/
-  # Rails 3
-  
-  ActionDispatch::Routing::Mapper.class_eval do
-    protected
+ActionDispatch::Routing::Mapper.class_eval do
+  protected
 
-    def devise_harvard_auth_proxy_authenticatable(mapping, controllers)
-      
-      Rails.logger.warn('instantiating routes') if Devise.debug
-      Rails.logger.warn("Mapping: #{mapping.inspect}") if Devise.debug
-      Rails.logger.warn("Controllers: #{controllers.inspect}") if Devise.debug
+  def devise_harvard_auth_proxy_authenticatable(mapping, controllers)
+    
+    Rails.logger.warn('instantiating routes') if Devise.debug
+    Rails.logger.warn("Mapping: #{mapping.inspect}") if Devise.debug
+    Rails.logger.warn("Controllers: #{controllers.inspect}") if Devise.debug
 
-      get "hauthproxy/valid", :to => "#{controllers[:hauthproxy]}#valid"
-      get "hauthproxy/invalid", :to => "#{controllers[:hauthproxy]}#invalid"
-      get "hauthproxy/sign_in", :to => "#{controllers[:hauthproxy]}#sign_in"
-      delete "hauthproxy/sign_out", :to => "#{controllers[:hauthproxy]}#sign_out"
-      delete "hauthproxy/sign_out", :to => "#{controllers[:hauthproxy]}#sign_out", :as => :destroy_user_session
-    end
-  
+    get "hauthproxy/valid", :to => "#{controllers[:hauthproxy]}#valid"
+    get "hauthproxy/invalid", :to => "#{controllers[:hauthproxy]}#invalid"
+    get "hauthproxy/sign_in", :to => "#{controllers[:hauthproxy]}#sign_in"
+    delete "hauthproxy/sign_out", :to => "#{controllers[:hauthproxy]}#sign_out"
+    delete "hauthproxy/sign_out", :to => "#{controllers[:hauthproxy]}#sign_out", :as => :destroy_user_session
   end
-else
-  # Rails 2
-  
-  ActionController::Routing::RouteSet::Mapper.class_eval do
-    protected
-    def harvard_auth_proxy_authenticatable(routes, mapping)
-      # TODO
-    end
-  end
+
 end
